a4301fcefafb5cced4989a6813d7c269168e806c
Supernoodles99 <155268413+Supernoodles99@users.noreply.github.com>
2024-02-27 12:42:48 -0600

    fixed up some mistakes

--- dumbutils.js
+++ dumbutils.js
@@ -3,63 +3,61 @@ class dumbutils {
     return {
       id: 'dumbutils',
       name: 'DumbUtils',
-		color1: '#570098',
-		color2: '#290057',
-		color3: '#1e0040',
+      color1: '#570098',
+      color2: '#290057',
+      color3: '#1e0040',
       blocks: [
         {
           opcode: 'hello',
           blockType: Scratch.BlockType.REPORTER,
           text: 'Hello!'
         },
-		  {
-		    opcode: 'rpbtof',
-		  	 blockType: Scratch.BlockType.BOOLEAN,
-		  	 text: 'randomly pick between true or false'
-		  },
-		  {
-		    opcode: 'yearequals',
-		  	 blockType: Scratch.BlockType.BOOLEAN,
-		  	 text: 'is it [YEAR]?',
-			 arguments: {
-			 YEAR: {
-			 	type: Scratch.ArgumentType.NUMBER,
-				defaultValue: 2024
-				},
-			 }
-		  }
-		  {
-		    opcode: 'divideself',
-		  	 blockType: Scratch.BlockType.REPORTER,
-		  	 text: 'divide [NUM] by itself',
-			 arguments: {
-			 NUM: {
-			 		type: Scratch.ArgumentType.NUMBER,
-					defaultValue: 1
-				}
-			 }
-		  }
+	{
+	  opcode: 'rpbtof',
+    	  blockType: Scratch.BlockType.BOOLEAN,
+          text: 'randomly pick between true or false'
+	},
+	{
+	  opcode: 'yearequals',
+    	  blockType: Scratch.BlockType.BOOLEAN,
+	  text: 'is it [YEAR]?',
+	  arguments: {
+	   YEAR: {
+		type: Scratch.ArgumentType.NUMBER,
+		defaultValue: 2024
+		 },
+	  }
+	},
+	{
+          opcode: 'divideself',
+	  blockType: Scratch.BlockType.REPORTER,
+	  text: 'divide [NUM] by itself',
+	  arguments: {
+	    NUM: {
+		type: Scratch.ArgumentType.NUMBER,
+		defaultValue: 1
+		 }
+	  }
+	}
       ]
-    };
-  }
+  };
 
   hello() {
     return 'hello world';
   }
   rpbtof() {
 	 const trueorfalse = [true, false];
-	 var random = Math.floor(Math.random() * trueorfalse.length);
-	 return trueorfalse[random]
-	 ;
+	 let random = Math.floor(Math.random() * 2);
+	 return trueorfalse[random];
   }
   yearequals(year) {
-  return year.YEAR == new Date().getFullYear()
+  	return year.YEAR === new Date().getFullYear();
   }
   divideself(num) {
-  if (num.NUM == 0) {
-  	return "aint no way you gonna break the universe 💀"
+  	if (num.NUM === 0) {
+  		return "aint no way you gonna break the universe 💀"
 	} else {
-	return 1
+		return 1
 	}
   }
 }
